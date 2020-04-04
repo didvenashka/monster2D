75147256368841247b33ffcdb48d34627a691c80
didvenashka <56407982+didvenashka@users.noreply.github.com>
2020-04-04 23:06:36 +0300

    Создание проекта
    Создание нового проекта с минимальным функционалом

---
+++ Monster/Assets/Scripts/Healthhelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthhelper : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int Health = 100;
+    // Start is called before the first frame update
+    public void GetHit(int damage)
+    {
+        int health = Health - damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        Health = health;
+        Debug.Log("Health=" + Health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Monster/Assets/Scripts/HitHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHelper : MonoBehaviour
+{
+   
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown()");
+        GetComponent<Animator>().SetTrigger("Hit");
+        GetComponent<Healthhelper>().GetHit(10);
+    }
+}
+
