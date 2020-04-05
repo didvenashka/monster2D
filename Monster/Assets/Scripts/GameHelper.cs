c124b323e4108350734d90e832073e1808f23637
didvenashka <56407982+didvenashka@users.noreply.github.com>
2020-04-05 17:28:23 +0300

    Gold
    Добавлен счётчик золота

---
+++ Monster/Assets/Scripts/GameHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHelper : MonoBehaviour
+{
+    public Text PlauerGoldUI;
+    public int PlayerGold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlauerGoldUI.text = PlayerGold.ToString();
+    }
+}
--- Monster/Assets/Scripts/Healthhelper.cs
+++ Monster/Assets/Scripts/Healthhelper.cs
@@ -6,12 +6,19 @@ public class Healthhelper : MonoBehaviour
 {
     public int MaxHealth = 100;
     public int Health = 100;
+    public int Gold = 5;
+    GameHelper _gameHelper;
+    void Start()
+    {
+        _gameHelper = GameObject.FindObjectOfType<GameHelper>();
+    }
     // Start is called before the first frame update
     public void GetHit(int damage)
     {
         int health = Health - damage;
         if (health <= 0)
         {
+            _gameHelper.PlayerGold += Gold;
             Destroy(gameObject);
         }
         Health = health;
